2417870eaeb13808642b7f193189a030051167ea
Siwat Ponpued <siwatponpued@gmail.com>
2022-01-26 16:51:18 +0700

    refactored

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,10 @@
 def is_leapyear(year: int)-> bool:
-    return (is_centuryyear(year) and year % 400 == 0) or year % 4 == 0
+    is_centuryyear_divisible_by_400 = is_centuryyear(year) and year % 400 == 0
+    is_divisible_by_4 = year % 4 == 0
+    
+    return is_centuryyear_divisible_by_400 or is_divisible_by_4
 
 def is_centuryyear(year:int)-> bool:
-    return year % 100 == 0
+    is_divisible_by_100 =  year % 100 == 0
+    
+    return is_divisible_by_100
