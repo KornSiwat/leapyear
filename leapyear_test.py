3afd5ef06b5b5e3e375420bc402f540d286ed02d
Siwat Ponpued <siwatponpued@gmail.com>
2022-01-26 16:38:59 +0700

    created test is leapyear when input is centuryyear divisible by 400

--- leapyear_test.py
+++ leapyear_test.py
@@ -3,3 +3,6 @@ from leapyear import is_leapyear, is_centuryyear
 class TestLeapyearClass:
     def test_is_centuryyear_when_input_is_divisible_by_100(self):
         assert is_centuryyear(2000) == True
+
+    def test_is_leapyear_when_input_is_centuryyear_divisible_by_400(self):
+        assert is_leapyear(2000) == True
