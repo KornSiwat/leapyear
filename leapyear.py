2b731a7381b8679707d84b800cb0d52743842550
Siwat Ponpued <siwatponpued@gmail.com>
2022-01-26 16:49:37 +0700

    implement is_leapyear to pass is not leapyear when input is centuryyear that is not divisble by 400 and not centuryyear that is not divisible by 4 test

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,5 @@
 def is_leapyear(year: int)-> bool:
-    return True
+    return (is_centuryyear(year) and year % 400 == 0) or year % 4 == 0
 
 def is_centuryyear(year:int)-> bool:
     return year % 100 == 0
